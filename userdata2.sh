a29ae32d372c7b0b6327e287f69ca7ea744245b9
Lincy Varghese <lincy.varghese@reancloud.com>
2017-11-17 18:44:03 +0530

    Update userdata2.sh

--- userdata2.sh
+++ userdata2.sh
@@ -5,5 +5,10 @@ sudo yum update -y
 #sudo yum install tree vim python27 atop jq wget curl python-pip git -y
 sudo yum install httpd -y
 sleep 10
-sudo yum install tree vim python27 atop jq wget curl python-pip git php7.0 -y
+sudo yum install tree vim python27 atop jq wget curl python-pip git php -y
 sleep 10
+sudo chkconfig httpd on
+sudo service httpd restart
+sleep 10
+sudo yum install mysql-server -y
+sudo yum install php php-mysql -y
